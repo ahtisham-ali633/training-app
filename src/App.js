9fb2e074731180c544039833e676bda75e85e4c0
ahtisham-ali633 <ahtisham@carbonteq.com>
2021-07-07 17:04:55 +0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+        <React.StrictMode>
+            <h1>hello teraining app</h1>
+        </React.StrictMode>
+  );
+}
+
+export default App
